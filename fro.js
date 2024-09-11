325a7cf1b5398c824a7a9a610442b5340544ea26
David <davidopotowsky14@gmail.com>
2024-09-11 17:14:23 -0500

    froIf&Else's

--- fro.js
+++ fro.js
@@ -3,11 +3,19 @@
 let input = froyo(flChoice);
 const flChoice=vanilla || Strawberry || coffee;
 
+
 if (input) {
     let flavors = input.split(',').map(flavor=> flavor.trim());
 
     let flavorCounts = {};
 }
-flavors.forEach(flavor){
+flavors.forEach(flavor) {
+    if (flavorCounts[flavor]){
+        flavorCounts[flavor]++;
+    } else {
+        flavorCounts[flavor] = 1;
+    }
+}
 
-})
+console.log(flavorCounts);
+else {}
