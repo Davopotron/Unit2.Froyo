22a4dc6b80737fdaeff11d592dcbf679743ea24b
David <davidopotowsky14@gmail.com>
2024-09-11 17:08:16 -0500

    froFLCt

--- fro.js
+++ fro.js
@@ -1,4 +1,13 @@
 
 //Choose flavor seperated by commas// 
-let input = froyo(flChoice)
+let input = froyo(flChoice);
+const flChoice=vanilla || Strawberry || coffee;
 
+if (input) {
+    let flavors = input.split(',').map(flavor=> flavor.trim());
+
+    let flavorCounts = {};
+}
+flavors.forEach(flavor){
+
+})
