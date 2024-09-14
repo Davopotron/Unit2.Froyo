937b43b5949fc26604ee73ffb083a34eebee280a
David <davidopotowsky14@gmail.com>
2024-09-14 12:19:00 -0500

    recorrFroyoAssgnmnt

--- fro.js
+++ fro.js
@@ -4,18 +4,19 @@ let input = froyo(flChoice);
 const flChoice=vanilla || Strawberry || coffee;
 
 
+
 if (input) {
     let flavors = input.split(',').map(flavor=> flavor.trim());
 
     let flavorCounts = {};
 }
-flavors.forEach(flavor) {
+flavors.forEach(flChoice) {
     if (flavorCounts[flavor]){
         flavorCounts[flavor]++;
     } else {
-        flavorCounts[flavor] = 1;
+        flavorCounts[flavor] += 1;
     }
+    return flChoice
 }
 
 console.log(flavorCounts);
-return flChoice
---
+++ map.js
@@ -0,0 +1,22 @@
+/*
+Map: transform one array into another of the same length
+transformFn: a function that takes an element as input & returns another element
+
+const double = n => n * 2;
+map([1,2,3,4,5], double) >>> [2,4,6,8,10]
+
+const triple = n => n * 3;
+map([1,2,3,4,5], triple) >>> [3,6,9,12,15]
+*/
+
+const transformFn = n => n/2;
+const arr = [2,4,6,8];
+function map(arr, transformFn) {
+    // TODO: odd number rooms
+
+   
+   console.log(map)
+
+}
+
+
